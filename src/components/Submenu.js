e7195e61478939ceb4a3a0cf24d04597fe669c74
Sabuhi Teymurov <teymurov.sebuhi740@gmail.com>
2022-06-16 10:53:22 +0400

    Implemented dynamic menu columns

--- src/components/Submenu.js
+++ src/components/Submenu.js
@@ -7,13 +7,22 @@ const Submenu = () => {
     location,
     page: { page, links },
   } = useGlobalContext();
+
   const container = useRef(null);
+  const [columns, setColumns] = useState('col-2');
+
   useEffect(() => {
+    setColumns('col-2');
+
     const submenu = container.current;
     const { center, bottom } = location;
     submenu.style.left = `${center}px`;
     submenu.style.top = `${bottom}px`;
-  }, [location]);
+
+    if (links.length === 3) setColumns('col-3');
+
+    if (links.length > 3) setColumns('col-4');
+  }, [location, links]);
 
   return (
     <aside
@@ -21,7 +30,7 @@ const Submenu = () => {
       ref={container}
     >
       <h4>{page}</h4>
-      <div className={`submenu-center col-2`}>
+      <div className={`submenu-center ${columns}`}>
         {links.map((link, index) => {
           const { label, icon, url } = link;
           return (
